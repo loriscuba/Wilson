ef516d2f2b8529731d8f8d54376801bce091aadc
loriscuba <34890171+loriscuba@users.noreply.github.com>
2026-05-15 15:44:30 +0200

    Update env.js

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 window.__env = {
     SUPABASE_URL: 'https://btcnfxoqkddpfhzahhaz.supabase.co',
     SUPABASE_KEY: 'sb_publishable_bppoTX0oooogt3v2qj6zbQ_XSSyOpY2',
-    GITHUB_TOKEN: '' // aggiunto in localStorage dal browser al primo sync
+    GITHUB_TOKEN: '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' // aggiunto in localStorage dal browser al primo sync
 };
